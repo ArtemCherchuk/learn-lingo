f5135fc667e24166cd0935c6f0ab4595433786f5
ArtemCherchuk <250artem250@gmail.com>
2024-07-08 10:50:25 +0200

    add styles media 1440

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,36 +4,38 @@ const Hero = () => {
   return (
     <div className={css.containerHero}>
       <div className={css.containerUnlock}>
-        <h1 className={css.title}>
-          Unlock your potential with the best{' '}
-          <span className={css.spanLanguage}>language</span> tutors
-        </h1>
-        <p className={css.text}>
-          Embark on an Exciting Language Journey with Expert Language Tutors:
-          Elevate your language proficiency to new heights by connecting with
-          highly qualified and experienced tutors.
-        </p>
-        <button type="button" className={css.button}>
-          Get started
-        </button>
+        <div className={css.containerUnlockText}>
+          <h1 className={css.title}>
+            Unlock your potential with the best{' '}
+            <span className={css.spanLanguage}>language</span> tutors
+          </h1>
+          <p className={css.text}>
+            Embark on an Exciting Language Journey with Expert Language Tutors:
+            Elevate your language proficiency to new heights by connecting with
+            highly qualified and experienced tutors.
+          </p>
+          <button type="button" className={css.button}>
+            Get started
+          </button>
+        </div>
       </div>
       <div className={css.containerImg}></div>
       <div className={css.containerList}>
         <ul className={css.list}>
           <li className={css.itemList}>
-            <p className={css.textList}>32,000 +</p>
+            <p className={css.textList}>32,000 &#43;</p>
             <span className={css.spanList}>Experienced tutors</span>
           </li>
           <li className={css.itemList}>
-            <p className={css.textList}>300,000 +</p>
+            <p className={css.textList}>300,000 &#43;</p>
             <span className={css.spanList}>5-star tutor reviews</span>
           </li>
           <li className={css.itemList}>
-            <p className={css.textList}>120 +</p>
+            <p className={css.textList}>120 &#43;</p>
             <span className={css.spanList}>Subjects taught</span>
           </li>
           <li className={css.itemList}>
-            <p className={css.textList}>200 +</p>
+            <p className={css.textList}>200 &#43;</p>
             <span className={css.spanList}>Tutor nationalities</span>
           </li>
         </ul>
